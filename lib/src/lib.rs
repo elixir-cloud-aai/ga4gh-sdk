7cf9dfd323f7095cba4d9780321b6b737c789ef8
aaravm <aarav.mehta.mat22@itbhu.ac.in>
2024-06-07 15:39:36 +0530

    Initial commit

---
+++ lib/src/lib.rs
@@ -0,0 +1,14 @@
+//! This is the main library file for the `lib` crate.
+#![warn(missing_docs)]
+
+
+// UNIT TESTING
+#[cfg(test)]
+mod tests {
+    // use super::*;
+
+    #[test]
+    fn test() {
+        // This test currently does nothing, it is just a placeholder.
+    }
+}
